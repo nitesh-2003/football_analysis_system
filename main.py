42c55fdd3bfe415dbdb7efa8af3e04fc574b87bb
nitesh-2003 <niteeshkumar613@gmail.com>
2025-05-27 15:25:42 +0530

    Update tracking system on players and football

---
+++ main.py
@@ -0,0 +1,25 @@
+from utils import read_video, save_video
+from trackers import Tracker
+
+def main():
+    # Read video
+    video_frames = read_video('input_videos/match.mp4')
+
+    # Initialize Tracker
+    tracker = Tracker('models/best.pt')
+
+    # Get or compute tracks
+    tracks = tracker.get_object_tracks(
+        video_frames, 
+        read_from_stub=True,
+        stub_path='stubs/track_stubs.pkl'
+    )
+
+    # Draw annotations
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+
+    # Save video
+    save_video(output_video_frames, 'output_videos/output_video.avi')
+
+if __name__ == '__main__':
+    main()
